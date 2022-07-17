2a9cb1b68d023f81de5d3deb3ad3de15d8d5a625
Marcus Vinicius Leite <marcus.leite@solutis.net.br>
2022-07-16 21:31:20 -0300

    feat: nft functions

---
+++ __tests__/app/controllers/NFTController.test.ts
@@ -0,0 +1,33 @@
+import { calculateNFTMeasurements, calculatePixelsQuantity, createNFT } from '../../../app/controllers/NFTController';
+
+describe("test NFT controller funtions", () => {
+
+  it('should return the correct calc result calculatePixelsQuantity', () => {
+    expect(calculatePixelsQuantity(10)).toBe(800);
+    expect(calculatePixelsQuantity(12)).toBe(1152);
+    expect(calculatePixelsQuantity(14)).toBe(1568);
+  })
+
+  it('should calculate NFT Measurements', () => {
+    expect(calculateNFTMeasurements(12, 20)).toStrictEqual({NFTWidth: 720, chunkWidth: 240})
+  })
+
+  it('should create NFT object', () => {
+    const nft = createNFT(12);
+    expect(nft).toBeTruthy();
+    expect(nft.chunks).toBeTruthy();
+    if(nft && nft.chunks)
+      expect(nft.chunks.length).toBe(8);
+      expect(nft.chunkSize).toBe(12);
+      const chunk = nft.chunks[0];
+      expect(chunk).toBeTruthy;
+      expect(chunk.pixels.length).toBe(144);
+      expect(chunk.position).toBe(0);
+      const pixel = chunk.pixels[0];
+      expect(pixel).toBeTruthy();
+      expect(pixel.color).toBe('#FFFFFF');
+      expect(pixel.isAvailible).toBeTruthy();
+      expect(pixel.uuid.length).toBe(36);
+  })
+
+})
